f0037d07698c3a9c56cc425325a20485c9bdd15a
phong <duyphongptit1234@gmail.com>
2019-05-22 20:16:02 +0700

    phong

---
+++ funstring2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+$replace=str_replace("ha noi","hai duong","chao mung den thanh pho hai duong");
+echo $replace;
+?>
+</body>
+</html>
---
+++ operater.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+$a=5;
+$b=2;
+$tong= $a+$b;
+$hieu=$a-$b;
+$tich=$a*$b;
+$thuong=$a/$b;
+$laydu=$a%$b;
+echo "<br>tổng của 2 số " . $tong;
+echo "<br>hiệu của 2 số " . $hieu;
+echo "<br>tích của 2 số " . $tich;
+echo "<br>thương của 2 số " . $thuong;
+echo "<br>phép chia lấy dư của 2 số " . $laydu;
+?>
+</body>
+</html>
